7ee2808e42c325bc6b48162db4a0091ae078cdd8
SergioHenrique23 <serginho.abreu@outlook.com>
2023-10-11 08:44:39 -0300

    criptografia de senha

--- src/main/java/br/com/sergiohenrique/todolist/user/UserController.java
+++ src/main/java/br/com/sergiohenrique/todolist/user/UserController.java
@@ -1,12 +1,16 @@
 package br.com.sergiohenrique.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
@@ -14,11 +18,26 @@ import io.micrometer.core.ipc.http.HttpSender.Response;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository
+    private UserRepository userRepository;
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+        
+        //Regra de negocio, migrar para service
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        System.out.println(user);
+
+        if(!ObjectUtils.isEmpty(user)){
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já cadastrado");
+        }
+
+        var senhaCriptografada = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(senhaCriptografada);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
--- src/main/java/br/com/sergiohenrique/todolist/user/UserRepository.java
+++ src/main/java/br/com/sergiohenrique/todolist/user/UserRepository.java
@@ -3,8 +3,9 @@ package br.com.sergiohenrique.todolist.user;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
-    
-    
+    List<UserModel> findByUsername(String username);
 }
